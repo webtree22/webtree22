51eb35751e03f2b98fba30540b7456d83b828917
webtree22 <webtree22@gmail.com>
2017-01-29 13:36:40 +0545

    Update config.inc.php

--- php/config.inc.php
+++ php/config.inc.php
@@ -8,22 +8,22 @@ $config['use_smtp']				= true;						// true|false
 // SMTP Server Settings
 $config['smtp_host'] 			= 'smtp.gmail.com';   		// eg.: smtp.gmail.com
 $config['smtp_port'] 			= 587;						// eg.: 587
-$config['smtp_user'] 			= ''; 						// you@gmail.com
-$config['smtp_pass'] 			= '';						// password
+$config['smtp_user'] 			= 'webtree22@gmail.com'; 						// you@gmail.com
+$config['smtp_pass'] 			= 'gurung123';						// password
 $config['smtp_ssl']				= false;					// should remain false
 
 // Who receive all emails?
-$config['send_to']				= 'youremail@gmail.com';	// destination of all emails sent throught contact form
+$config['send_to']				= 'webtree22@gmail.com';	// destination of all emails sent throught contact form
 
 // Email Subject
-$config['subject']				= 'Smarty Contact Form';	// subject of emails you receive
+$config['subject']				= 'Webtree Contact Form';	// subject of emails you receive
 
 
 
 
 /** ******************************************************* MISC ******************************************************* **/
 define('UPLOAD_FOLDER',		'_upload/');										// upload folder path - slash at the end!
-define('UPLOAD_FOLDER_URL',	'http://localhost/PRJ/Smarty/HTML/php/_upload/');	// Full url path to upload folder (used for contact files) - slash at the end!
+define('UPLOAD_FOLDER_URL',	'http://webtree.com.np/php/_upload/');	// Full url path to upload folder (used for contact files) - slash at the end!
 define('UPLOAD_MAX_SIZE',	10000000);											// 1000000 = 10Mb
 
 
